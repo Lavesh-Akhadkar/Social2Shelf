a505c559c719dac1ac97cea5a72d0a31d49213ea
Lavesh Akhadkar <117550850+Lavesh-Akhadkar@users.noreply.github.com>
2025-07-29 15:11:08 +0530

    Update ProductDetailPage.jsx

--- web/src/Dummy Amazon/ProductDetailPage.jsx
+++ web/src/Dummy Amazon/ProductDetailPage.jsx
@@ -15,7 +15,7 @@ const ProductDetailsPage = () => {
         const fetchProductDetails = async () => {
             try {
                 const response = await axios.get(
-                    `https://d644-2405-201-20-b11e-a08d-24f7-5db0-803f.ngrok-free.app/api/products/${id}`,
+                    `https://social2shelf-dev.onrender.com/api/products/${id}`,
                     {
                         headers: {
                             "ngrok-skip-browser-warning": "69420",
